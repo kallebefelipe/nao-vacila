8b25c0e6cb08a1d8583c7da44c288fb7d09a37c2
lwallace843 <wallacesantana843@gmail.com>
2017-06-11 15:21:18 -0300

    projeto ionic

---
+++ projetao/src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
+  constructor(public navCtrl: NavController) {
+ 
+  }
+ 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-8.0543, -34.8813);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+  }
+
+}
