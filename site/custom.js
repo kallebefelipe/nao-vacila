112df94c53e18d171f616da03c264e225cd3843f
Juliana <julifs.13@gmail.com>
2017-06-20 23:59:54 -0300

    Mapa web

---
+++ site/custom.js
@@ -0,0 +1,46 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(-8.017655, -34.944377);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = 'marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Ocorrência</h3>' +
+                '<div class="info-content">' +
+                '<p>Tipo</p>'+
+                '<p>Data</p>'+
+                '<p>Endereço</p>'+
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
