b3af8316ff2cbdeda14354a4a70205b6ecbf079c
Kallebe Felipe <kallebefelipe@gmail.com>
2017-07-02 07:08:07 -0300

    Adcionando files tutorial

--- servidor/tutorial/tutorial/urls.py
+++ servidor/tutorial/tutorial/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from quickstart import views
+from django.conf.urls import url, include
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,4 +13,9 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
+
+urlpatterns = [
+    url(r'^', include('snippets.urls')),
 ]
