a075ac5f51f3768468f473a8e309e35ee9f7572f
Ana Carolina <a.caroolinasantos@gmail.com>
2017-07-13 03:38:19 -0300

    get de ocorrencia e marcação no mapa

--- naovacila-mobile/src/pages/home/home.ts
+++ naovacila-mobile/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { OcorrenciaServicoProvider } from '../../providers/ocorrencia-servico/ocorrencia-servico';
+import { Geolocation } from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -13,26 +14,21 @@ export class HomePage {
   map: any;
   ocorrencias: any;
  
-  constructor(public navCtrl: NavController, public ocorrenciaServico: OcorrenciaServicoProvider) {
-    this.ocorrenciaServico.carregarOcorrencias()
-      .subscribe(
-        data => {
-          this.ocorrencias = data;
-        },
-        err => {
-          console.log('ERROR ' + err);
-        },
-        ()=> console.log("serviço finalizado")
-      );
+  constructor(public navCtrl: NavController, public ocorrenciaServico: OcorrenciaServicoProvider, public geolocation: Geolocation) {
+    
   }
  
   ionViewDidLoad(){
     this.loadMap();
+    this.carregarOcorrencias();
+  
   }
  
   loadMap(){
+
+    this.geolocation.getCurrentPosition().then((position) => {
  
-    let latLng = new google.maps.LatLng(-8.0543, -34.8813);
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
  
     let mapOptions = {
       center: latLng,
@@ -45,7 +41,25 @@ export class HomePage {
     }
  
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
+    }, (err) =>{
+      console.log(err);
+    });
+  }
+
+  carregarOcorrencias(){
+    this.ocorrenciaServico.carregarOcorrencias()
+      .subscribe(
+        data => {
+          this.ocorrencias = data;
+          for(var i = 0; i<this.ocorrencias.length; i++){
+            this.adicionarMarcador(this.ocorrencias[i]);
+          }
+        },
+        err => {
+          console.log('ERROR ' + err);
+        },
+        ()=> console.log("serviço finalizado")
+      );
   }
 
   abrirRegistrarOcorrencia(){
@@ -54,6 +68,31 @@ export class HomePage {
 
   abrirCriarRota(){
     this.navCtrl.push('CriarRotaPage');
+  }
+
+  addInfoWindow(marker, content){
+ 
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
+
+  adicionarMarcador(ocorrencia){
+    console.log("INFO-adicionando marcador para ocorrencia: " + ocorrencia.titulo);
+    let latLng = new google.maps.LatLng(-8.044916, -34.9282);
+    let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+ 
+      let content = "<h4>"+ocorrencia.titulo+ "</h4> <p>" +ocorrencia.descricao+ "</p>" ;          
+ 
+      this.addInfoWindow(marker, content);
   }
 
 }
