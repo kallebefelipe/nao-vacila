d7eee21764425183472f6daefb2e6a3269fd54c3
Ana Carolina <a.caroolinasantos@gmail.com>
2017-08-01 04:04:30 -0300

    api noticias

---
+++ api-noticias/api-noticias/api-noticias/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace api_noticias.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api-noticias/api-noticias/api-noticias/Models/NoticiaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_noticias.Models
+{
+    public class NoticiaModel
+    {
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public string Data { get; set; }
+        public string Hora { get; set; }
+        public string UrlNoticia { get; set; }
+        public string UrlImagem { get; set; }
+    }
+}
