89533774d74ed01359b69f1aea85a90d87e544c7
Juliana <julifs.13@gmail.com>
2017-08-02 15:30:41 -0300

    Atualização estatísticas

--- site/js/estatisticas.js
+++ site/js/estatisticas.js
@@ -1,26 +1,43 @@
-var estatisticasData = new Array();
-var bairros = new Array();
-var quantidadeOcorrencias = new Array();
+var estatisticasBairroData = new Array();
+var estatisticasTipoData = new Array();
+var estatisticasAnoData = new Array();
 
-//GET JSON ESTATISTICAS
-$.getJSON( "https://webserver-nao-vacila.herokuapp.com/estatisticas/", function( data ) {
-    estatisticasData = data;
+var anos = new Array();
+var tipos = new Array();
+var bairros = new Array();
+var quantidadeOcorrencias = new Array();//talvez nao use
+
+//GET JSON ESTATISTICAS BAIRRO
+/*$.getJSON( "https://webserver-nao-vacila.herokuapp.com/estatisticas/", function( data ) {
+    estatisticasBairroData = data;
 	bairrosOcorrencias();
 })
+
+//GET JSON ESTATISTICAS TIPO
+$.getJSON( "site", function( data ) {
+    estatisticasTipoData = data;
+	tipoOcorrencias();
+})
+
+//GET JSON ESTATISTICAS ANO
+$.getJSON( "site", function( data ) {
+    estatisticasAnoData = data;
+	anoOcorrencias();
+})*/
 
 window.onload = function () {
 	var chartMes = new CanvasJS.Chart("chartContainerMes",
         {
 
           title:{
-          text: "Ocorrencias - por mês"
+          text: "Ocorrencias - por ano"
           },
            data: [
           {
             type: "line",
 
             dataPoints: [
-            { x: new Date(2011, 11, 31), y: 450 },
+            { x: new Date(2011, 11, 31), y: 450 },//tipos[0]
             { x: new Date(2012, 11, 31), y: 414 },
             { x: new Date(2013, 11, 31), y: 520 },
             { x: new Date(2014, 11, 31), y: 460 },
@@ -45,10 +62,9 @@ window.onload = function () {
                 type: "pie",
                 showInLegend: true,
                 toolTipContent: "{y} - #percent %",
-                yValueFormatString: "#0.#,,. Million",
                 legendText: "{indexLabel}",
                 dataPoints: [
-                    {  y: 4181563, indexLabel: "Roubo" },
+                    {  y: 4181563, indexLabel: "Roubo" },//anos[0]
                     {  y: 2175498, indexLabel: "Furto" },
                     {  y: 3125844, indexLabel: "Sequestro" },
                     {  y: 1176121, indexLabel: "Arrombamento"},
@@ -65,26 +81,34 @@ window.onload = function () {
 function bairrosOcorrencias() {//FALTA TESTAR
 	var maior = 0;
 	var posicaoBairro;
-	for(var x = 0; x < 5; x++){
-		for (var i = 0; i < estatisticasData.length; i++){
-			var numeroOcorrencias = estatisticasData[i].quantidade;
+	for(var x = 0; x < 5; x++){//Pega os 5 bairros com mais ocorrências
+		for (var i = 0; i < estatisticasBairroData.length; i++){
+			var numeroOcorrencias = estatisticasBairroData[i].quantidade;
 			if(numeroOcorrencias >= maior){
 				posicaoBairro = i;
 				maior = numeroOcorrencias;
 			}
 		}
-		bairros[x] = estatisticasData[posicaoBairro].bairro;
-		quantidadeOcorrencias[x] = estatisticasData[posicaoBairro].quantidade;
+		bairros[x] = estatisticasBairroData[posicaoBairro].bairro;
+		quantidadeOcorrencias[x] = estatisticasBairroData[posicaoBairro].quantidade;
 	}
-	criarTabela();
-}
-
-function criarTabela() {
-	for(var i = 0; i < bairros.length; i++){
+	for(var i = 0; i < bairros.length; i++){//Coloca os 5 bairros com mais ocorrências numa tabela
 		var tr = document.createElement("tr");
 		var td = document.createElement("td");
 		var bairro = document.createTextNode(bairros[i]);
 		tr.appendChild(td).appendChild(bairro);
 		document.getElementById('bairros').appendChild(tr);
+	}
+}
+
+function anoOcorrencias(){
+	for(var i = 0; i < estatisticasAnoData.length; i++){
+		anos[i] = estatisticasAnoData[i].quantidade;
+	}
+}
+
+function tipoOcorrencias(){
+	for(var i = 0; i < estatisticasTipoData.length; i++){
+		tipos[i] = estatisticasTipoData[i].quantidade;
 	}
 }
