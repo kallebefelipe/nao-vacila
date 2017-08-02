14a75046057c36b01837ae4f7a35caca78320ab8
Ana Carolina <a.caroolinasantos@gmail.com>
2017-08-02 03:03:48 -0300

    feed de noticias do app

--- naovacila-mobile/src/pages/noticias/noticias.ts
+++ naovacila-mobile/src/pages/noticias/noticias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NoticiaProvider } from '../../providers/noticia/noticia';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 /**
  * Generated class for the NoticiasPage page.
@@ -14,11 +16,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NoticiasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loader: any;
+  public noticias: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public noticiaServico: NoticiaProvider
+    , public loadingCtrl: LoadingController, private iab: InAppBrowser) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NoticiasPage');
+
+    this.presentLoading();
+    this.noticiaServico.carregarNoticias()
+      .subscribe(
+      data => {
+        this.noticias = data;
+        this.loader.dismiss();
+      },
+      err => {
+        console.log('[ERROR] ' + err);
+        this.loader.dismiss();
+      },
+      () => console.log("noticias carregadas")
+      );
+  }
+
+  abrirUrl(url) {
+    console.log('abrir noticia');
+    const noticia = this.iab.create(url, "_self", "location=yes");
+    noticia.show();
+  }
+
+  presentLoading() {
+
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando notícias..."
+    });
+
+    this.loader.present();
+
   }
 
 }
---
+++ naovacila-mobile/src/providers/noticia/noticia.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+let apiNoticiaUrl = "http://localhost:8100/api/noticia";
+
+@Injectable()
+export class NoticiaProvider {
+
+  constructor(public http: Http) {
+    console.log('Hello NoticiaProvider Provider');
+  }
+
+  carregarNoticias(){
+    return this.http.get(apiNoticiaUrl)
+      .map(res=>res.json())
+      .catch(res=>{return Observable.throw(res)});
+  }
+
+}
