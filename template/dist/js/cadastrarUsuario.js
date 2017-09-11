8bb317fa146e5221aa694e689952ca84c3457b28
Danilo Monteiro <danilo.smti93@gmail.com>
2017-09-11 02:00:51 -0300

    Cadastro de Usuário Finalizado

--- template/dist/js/cadastrarUsuario.js
+++ template/dist/js/cadastrarUsuario.js
@@ -1,26 +1,30 @@
 var cadastro;
+var tok = '123';
+var testando = null;
 
 $("form").submit(function(event) {
 
     cadastro = JSON.stringify({
-        token: null,
-        id_google: null
+        token: tok,
+        id_google: testando,
         email: $("#email").val(),
         sexo: $("#sexo").val(),
         nome: $("#nome").val() + $("#snome").val(),
         senha: $("#senha").val(),
-        url_foto: null
-        id_fb: null
+        url_foto: testando,
+        id_fb: testando,
         cidade: $("#endereco").val(),
-        id: localStorage.getItem("id")
+        id: localStorage.getItem("id_usuario")
+    });
 
-        $.ajax({
+    $.ajax({
         type: "POST",
         url: "https://webserver-nao-vacila.herokuapp.com/usuario/",
         data: cadastro,
         dataType: "json",
         contentType: "application/json"
-        });
-    });
-});
 
+    });
+
+    return confirm("Usuario cadastrado com sucesso");
+});
